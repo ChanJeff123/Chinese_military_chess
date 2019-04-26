d08262e725b06d225f8da80260293e3584b169b5
ChanJeff123 <45112288+ChanJeff123@users.noreply.github.com>
2019-04-27 00:24:47 +0800

    mix v1

--- arduino_mfrc522/2reader.c
+++ arduino_mfrc522/2reader.c
@@ -1,45 +1,29 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-#define RST_PIN                9          // Configurable, see typical pin layout above
-#define SS_1_PIN              10          // Configurable, see typical pin layout above
-#define SS_2_PIN               8          // Configurable, see typical pin layout above
-#define TAG_LED_1_PIN          5          // COnfigurable, authorized card = blinking LED tag1
-#define TAG_LED_2_PIN          6          // COnfigurable, authorized card = blinking LED tag2
-#define READER_LED_1_PIN       4          // COnfigurable, identification by reader = blinking LED reader1
-#define READER_LED_2_PIN       3          // COnfigurable, identification by reader = blinking LED reader2
-
-#define NR_OF_READERS   2         // number of RC522 modules
-
+int b1 = 0;
+int b2 = 0;
+int red_w = 3;
+int yellow_w = 4;
+#define RST_PIN   9
+#define SS_1_PIN  10
+#define SS_2_PIN  8
+#define NR_OF_READERS   2    // number of RC522 modules
+//void SUANFA();
 byte ssPins[] = {SS_1_PIN, SS_2_PIN};
-
 MFRC522 mfrc522[NR_OF_READERS];   // Create MFRC522 instance.
 
-/**
-   Initialize.
-*/
 void setup() {
-
-  pinMode(TAG_LED_1_PIN, OUTPUT); //Set TAG_LED_1_PIN as an output
-  pinMode(TAG_LED_2_PIN, OUTPUT); //Set TAG_LED_2_PIN as an output
-  pinMode(READER_LED_1_PIN, OUTPUT); //Set READER_LED_1_PIN as an output
-  pinMode(READER_LED_2_PIN, OUTPUT); //Set READER_LED_2_PIN as an output
-  Serial.begin(9600);       // Initialize serial communications with the PC
-  SPI.begin();        // Init SPI bus
-
+  Serial.begin(9600);
+  SPI.begin();
   for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
     mfrc522[reader].PCD_Init(ssPins[reader], RST_PIN); // Init each MFRC522 card
   }
-  Serial.println("Waiting for Tag ....");
+  Serial.println("welcome! pls put your TAGS in the reader");
   Serial.println();
-
 }
 
-/**
-   Main loop.
-*/
-void loop() {
-
+int SCAN_rtags(int b2) {
   for (uint8_t reader = 0; reader < NR_OF_READERS; reader++) {
     // Look for new cards
 
@@ -61,12 +45,6 @@ void loop() {
           Serial.println("Tag 1 ");
           Serial.print(card.substring(1));
           Serial.println();
-          digitalWrite (READER_LED_1_PIN, HIGH) ;    // Reader 1 LED On
-          digitalWrite (TAG_LED_1_PIN, HIGH) ;       // Tag 1 LED On
-          delay (2000) ;
-          digitalWrite (READER_LED_1_PIN, LOW) ;     // Reader 1 LED Off
-          digitalWrite (TAG_LED_1_PIN, LOW) ;        // Tag 1 LED Off
-          delay (200) ;
         }
         else if (reader == 1)                       //identification Tag 1 for reader 2
         {
@@ -74,12 +52,6 @@ void loop() {
           Serial.println("Tag 1 ");
           Serial.print(card.substring(1));
           Serial.println();
-          digitalWrite (READER_LED_2_PIN, HIGH) ;    // Reader 2 LED On
-          digitalWrite (TAG_LED_1_PIN, HIGH) ;       // Tag 1 LED On
-          delay (2000) ;
-          digitalWrite (READER_LED_2_PIN, LOW) ;     // Reader 2 LED Off
-          digitalWrite (TAG_LED_1_PIN, LOW) ;        // Tag 1 LED Off
-          delay (200) ;
         }
         delay(300);
       }
@@ -90,12 +62,6 @@ void loop() {
           Serial.println("Tag 2 ");
           Serial.print(card.substring(1));
           Serial.println();
-          digitalWrite (READER_LED_1_PIN, HIGH) ;    // Reader 1 LED On
-          digitalWrite (TAG_LED_2_PIN, HIGH) ;       // Tag 2 LED On
-          delay (2000) ;
-          digitalWrite (READER_LED_1_PIN, LOW) ;     // Reader 1 LED Off
-          digitalWrite (TAG_LED_2_PIN, LOW) ;        // Tag 2 LED Off
-          delay (200) ;
         }
         else if (reader == 1)                       //identification Tag 2 for reader 2
         {
@@ -103,12 +69,6 @@ void loop() {
           Serial.println("Tag 2 ");
           Serial.print(card.substring(1));
           Serial.println();
-          digitalWrite (READER_LED_2_PIN, HIGH) ;    // Reader 2 LED On
-          digitalWrite (TAG_LED_2_PIN, HIGH) ;       // Tag 2 LED On
-          delay (2000) ;
-          digitalWrite (READER_LED_2_PIN, LOW) ;     // Reader 2 LED Off
-          digitalWrite (TAG_LED_2_PIN, LOW) ;        // Tag 2 LED Off
-          delay (200) ;
         }
         delay(300);
       }
@@ -131,16 +91,110 @@ void loop() {
       mfrc522[reader].PICC_HaltA();
       // Stop encryption on PCD
       mfrc522[reader].PCD_StopCrypto1();
-    } //if (mfrc522[reader].PICC_IsNewC
-  } //for(uint8_t reader
-}
-
-/**
-   Helper routine to dump a byte array as hex values to Serial.
-*/
-void dump_byte_array(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-    Serial.print(buffer[i], HEX);
+    }
   }
 }
+  void SUANFA() {
+    //比較大小，普通的情况
+    if (b1 == 0 || b2 == 0) {
+      Serial.println("no enough card");
+    }
+    else if (b1 < 10 && b2 < 10 && b1 < b2) {
+      b1_win();
+    } else if (b1 < 10 && b2 < 10 && b1 > b2) {
+      b2_win();
+    } else if (b1 < 10 && b2 < 10 && b1 == b2) {
+      no_win();
+      //吃掉军棋的情况
+    } else if (b1 == 12) {
+      end_ywin();
+    } else if (b2 == 12) {
+      end_rwin();
+      //炸弹的情况
+    } else if (b1 == 11 || b2 == 11) {
+      no_win();
+      //地雷的情况
+    } else if (b1 == 10) {
+      if (b2 == 9) {
+        b2_win();
+      } else if (b2 == 11) {
+        no_win();
+      } else {
+        b1_win();
+      }
+    } else if (b2 == 10) {
+      if (b1 == 9) {
+        b1_win();
+      } else if (b1 == 11) {
+        no_win();
+      } else {
+        b2_win();
+      }
+    }
+  }
+
+  void b1_win() {
+    Serial.println("red side win");
+    digitalWrite(red_w, HIGH);
+    delay(1000);
+    return 0;
+  }
+  void b2_win() {
+    Serial.println("yellow side win");
+    digitalWrite(yellow_w, HIGH);
+    delay(1000);
+    return b1 = 0;
+  }
+  void no_win() {
+    Serial.println("all out");
+    digitalWrite(yellow_w, HIGH);
+    digitalWrite(red_w, HIGH);
+    delay(1000);
+    return 0;
+  }
+  void end_rwin() {
+    Serial.println("game over redside win");
+    digitalWrite(red_w, HIGH);
+    delay(400);
+    digitalWrite(red_w, LOW);
+    delay(400);
+    digitalWrite(red_w, HIGH);
+    delay(400);
+    digitalWrite(red_w, LOW);
+    delay(400);
+    digitalWrite(red_w, HIGH);
+    delay(400);
+    digitalWrite(red_w, LOW);
+    return 0;
+  }
+  void end_ywin() {
+    Serial.println("game over yellowside win");
+    digitalWrite(yellow_w, HIGH);
+    delay(400);
+    digitalWrite(yellow_w, LOW);
+    delay(400);
+    digitalWrite(yellow_w, HIGH);
+    delay(400);
+    digitalWrite(yellow_w, LOW);
+    delay(400);
+    digitalWrite(yellow_w, HIGH);
+    delay(400);
+    digitalWrite(yellow_w, LOW);
+    return 0;
+  }
+  void ERROR_GAME() {
+    Serial.println("game ERROR!!");
+    return 0;
+  }
+  void loop() {
+    SUANFA();
+  }
+  /**
+     Helper routine to dump a byte array as hex values to Serial.
+  */
+  void dump_byte_array(byte * buffer, byte bufferSize) {
+    for (byte i = 0; i < bufferSize; i++) {
+      Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+      Serial.print(buffer[i], HEX);
+    }
+  }
