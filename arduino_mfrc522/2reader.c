abf9f7185affbc39cc23b02a7eac5259ba861a0d
ChanJeff123 <45112288+ChanJeff123@users.noreply.github.com>
2019-04-27 01:32:41 +0800

    TEST 带改进

--- arduino_mfrc522/2reader.c
+++ arduino_mfrc522/2reader.c
@@ -1,10 +1,12 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-int b1 = 0;
-int b2 = 0;
+int A;
+int B = 0;
+int SUM_C= B*100+B2;
 int red_w = 3;
 int yellow_w = 4;
+
 #define RST_PIN   9
 #define SS_1_PIN  10
 #define SS_2_PIN  8
@@ -94,107 +96,119 @@ int SCAN_rtags(int b2) {
     }
   }
 }
-  void SUANFA() {
-    //比較大小，普通的情况
-    if (b1 == 0 || b2 == 0) {
-      Serial.println("no enough card");
-    }
-    else if (b1 < 10 && b2 < 10 && b1 < b2) {
-      b1_win();
-    } else if (b1 < 10 && b2 < 10 && b1 > b2) {
+void SUANFA() {
+  //比較大小，普通的情况
+  if (b1 == 0 || b2 == 0) {
+    Serial.println("no enough card");
+  }
+  else if (b1 < 10 && b2 < 10 && b1 < b2) {
+    b1_win();
+  } else if (b1 < 10 && b2 < 10 && b1 > b2) {
+    b2_win();
+  } else if (b1 < 10 && b2 < 10 && b1 == b2) {
+    no_win();
+    //吃掉军棋的情况
+  } else if (b1 == 12) {
+    end_ywin();
+  } else if (b2 == 12) {
+    end_rwin();
+    //炸弹的情况
+  } else if (b1 == 11 || b2 == 11) {
+    no_win();
+    //地雷的情况
+  } else if (b1 == 10) {
+    if (b2 == 9) {
       b2_win();
-    } else if (b1 < 10 && b2 < 10 && b1 == b2) {
+    } else if (b2 == 11) {
       no_win();
-      //吃掉军棋的情况
-    } else if (b1 == 12) {
-      end_ywin();
-    } else if (b2 == 12) {
-      end_rwin();
-      //炸弹的情况
-    } else if (b1 == 11 || b2 == 11) {
+    } else {
+      b1_win();
+    }
+  } else if (b2 == 10) {
+    if (b1 == 9) {
+      b1_win();
+    } else if (b1 == 11) {
       no_win();
-      //地雷的情况
-    } else if (b1 == 10) {
-      if (b2 == 9) {
-        b2_win();
-      } else if (b2 == 11) {
-        no_win();
-      } else {
-        b1_win();
-      }
-    } else if (b2 == 10) {
-      if (b1 == 9) {
-        b1_win();
-      } else if (b1 == 11) {
-        no_win();
-      } else {
-        b2_win();
-      }
+    } else {
+      b2_win();
     }
   }
+}
+//一起出局的情况
 
-  void b1_win() {
-    Serial.println("red side win");
-    digitalWrite(red_w, HIGH);
-    delay(1000);
-    return 0;
-  }
-  void b2_win() {
-    Serial.println("yellow side win");
-    digitalWrite(yellow_w, HIGH);
-    delay(1000);
-    return b1 = 0;
-  }
-  void no_win() {
-    Serial.println("all out");
-    digitalWrite(yellow_w, HIGH);
-    digitalWrite(red_w, HIGH);
-    delay(1000);
-    return 0;
-  }
-  void end_rwin() {
-    Serial.println("game over redside win");
+if (((SUM_C %101==0) &&(SUM_C /101<10)) ||((SUM_C % 100 == 11)&&(SUM_C /100<12))\
+||((SUM_C / 100 == 11)&&(SUM_C <1113))){
+    A=1;
+    return A;
+}
+else if (102,103,104,105,106,107,108,109,203,\
+204,205,206,207,208,209,304,305,306,307,308,\
+309,405,406,407,408,409,506,507,508,509,607,\
+608,609,708,709,809,910) {
+    A=2;
+    return A;
+}
+else if (102,103,104,105,106,107,108,109,203,\
+204,205,206,207,208,209,304,305,306,307,308,\
+309,405,406,407,408,409,506,507,508,509,607,\
+608,609,708,709,809,910) {
+    A=2;
+    return A;
+}
+
+{
+    /* code */
+}
+
+switch (A)
+{
+case 1:
+  Serial.println("all out");
+  digitalWrite(yellow_w, HIGH);
+  digitalWrite(red_w, HIGH);
+  delay(1000);
+  break;
+case 2:
+  Serial.println("red side win");
+  digitalWrite(red_w, HIGH);
+  delay(1000);
+  break;
+case 3:
+  Serial.println("yellow side win");
+  digitalWrite(yellow_w, HIGH);
+  delay(1000);
+  break;
+case 4:
+  Serial.println("game over redside win");
+  for (i = 1; i <= 5; i++) {
     digitalWrite(red_w, HIGH);
     delay(400);
     digitalWrite(red_w, LOW);
     delay(400);
-    digitalWrite(red_w, HIGH);
-    delay(400);
-    digitalWrite(red_w, LOW);
-    delay(400);
-    digitalWrite(red_w, HIGH);
-    delay(400);
-    digitalWrite(red_w, LOW);
-    return 0;
   }
-  void end_ywin() {
-    Serial.println("game over yellowside win");
+  break;
+case 5:
+  Serial.println("game over yellowside win");
+  for (i = 1; i <= 5; i++) {
     digitalWrite(yellow_w, HIGH);
     delay(400);
     digitalWrite(yellow_w, LOW);
     delay(400);
-    digitalWrite(yellow_w, HIGH);
-    delay(400);
-    digitalWrite(yellow_w, LOW);
-    delay(400);
-    digitalWrite(yellow_w, HIGH);
-    delay(400);
-    digitalWrite(yellow_w, LOW);
-    return 0;
   }
-  void ERROR_GAME() {
-    Serial.println("game ERROR!!");
-    return 0;
+  break;
+default:
+  Serial.println("ERROR!!!");
+  break;
+}
+void loop() {
+  SUANFA();
+}
+/**
+   Helper routine to dump a byte array as hex values to Serial.
+*/
+void dump_byte_array(byte * buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
   }
-  void loop() {
-    SUANFA();
-  }
-  /**
-     Helper routine to dump a byte array as hex values to Serial.
-  */
-  void dump_byte_array(byte * buffer, byte bufferSize) {
-    for (byte i = 0; i < bufferSize; i++) {
-      Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-      Serial.print(buffer[i], HEX);
-    }
-  }
+}
